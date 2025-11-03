cbe8a9e853dcdf9c21f60b6fc30dea84ebe27474
mlouis13 <mlouis@edenschool.fr>
2025-11-03 17:27:22 +0100

    exercicejs 2

---
+++ ex06.js
@@ -0,0 +1,6 @@
+function getReduction(price, age) {
+	// Écrire le code ici
+}
+
+console.log(getReduction(10, 5)); // Résultat attendu: 0
+console.log(getReduction(150, 25)); // Résultat attendu: 45
